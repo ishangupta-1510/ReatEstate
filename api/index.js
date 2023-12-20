d51792fedad9c9b5f5e63414b793b75395a0f2f5
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2023-12-20 12:16:19 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("server running on 3000");
-})
+  console.log("server running on 3000");
+});
